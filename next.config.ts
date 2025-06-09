5477d81b722ee6e5d30be071d40c24246b3170c1
Spike433 <juhacar116@gmail.com>
2025-06-09 17:40:24 +0200

    Remove next.config.mjs and its configuration logic

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: false,
+  },
+  experimental: {
+    webpackBuildWorker: true,
+  },
+};
+
+export default nextConfig;
