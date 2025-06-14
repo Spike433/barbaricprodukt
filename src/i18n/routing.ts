43cb132f477f83f5c6fd97a85d1a61067f1730a4
Spike433 <juhacar116@gmail.com>
2025-06-14 11:24:56 +0200

    Update locale options and default locale in HR translations

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,8 +2,8 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['hr'],
+  locales: ['en', 'hr', 'de', 'fr', 'it', 'ch' ],
  
   // Used when no locale matches
-  defaultLocale: 'hr'
+  defaultLocale: 'en'
 });
