194806021a181cd64f75ff9e5a78973dc32e025e
Spike433 <juhacar116@gmail.com>
2025-09-06 11:25:10 +0200

    refactor: remove unused favicon links for cleaner layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -189,9 +189,6 @@ export default async function LocaleLayout({
     <html lang={locale} className="scroll-smooth">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#1f2937" />
         <meta name="format-detection" content="telephone=yes" />
